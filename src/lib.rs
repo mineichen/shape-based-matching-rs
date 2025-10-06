fdb4956291e65afab8bc3d66df20de0d8af091f7
Markus Ineichen <kusi.ineichen@gmail.com>
2025-10-07 00:00:42 +0200

    move match into submodule

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod image_buffer;
 mod line2dup;
+mod match_result;
 mod pyramid;
 mod simd_utils;
 
-pub use line2dup::{Detector, DetectorBuilder, Feature, Match};
+pub use line2dup::{Detector, DetectorBuilder, Feature};
+pub use match_result::Match;
